42b6bd649fc853522468b7806619444d2e778228
Neeya <169307919+nhasan6@users.noreply.github.com>
2025-08-01 12:31:39 -0400

    Add CrossOrigin

--- springblog/src/main/java/com/strawberry/springblog/PostController.java
+++ springblog/src/main/java/com/strawberry/springblog/PostController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/posts")
 public class PostController {
 
