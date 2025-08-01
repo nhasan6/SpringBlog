c171dd2588822bcccfec9e231a76f73e604f07cb
Neeya <169307919+nhasan6@users.noreply.github.com>
2025-08-01 12:38:33 -0400

    Update UserServiceInterface's modification method

--- springblog/src/main/java/com/strawberry/springblog/UserService.java
+++ springblog/src/main/java/com/strawberry/springblog/UserService.java
@@ -8,7 +8,7 @@ public interface UserService {
     public User addUser(String email, String password, String firstName, String lastName, String username);
     public User findUser(String username);
     public boolean hasAuthorization(User account);
-    public boolean modifyUser(User account, String field, String newContent);
+    public boolean modifyUser(User account, User updatedUser);
     public void deleteUser(User account);
     public User getCurrentUser();
     public boolean loginToUser(User lockedUser, String password);
